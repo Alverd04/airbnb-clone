208904de2213f93f017f45464db90864244e6179
Alverd04 <albert.font.fernandez@gmail.com>
2024-01-22 18:14:09 +0100

    create basic navbar

---
+++ app/components/Navbar/Logo.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <Image
+      src="/images/logo.png"
+      alt="logo"
+      className="hidden md:block cursor-pointer"
+      height={100}
+      width={100}
+    />
+  );
+};
+
+export default Logo;
